35200c0c72b4bdcbb77b02805f9efbf993159403
Casey Webster <cwebster@glgroup.com>
2022-12-12 15:29:29 -0600

    Day7!!

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -64,6 +64,38 @@ impl Node<ElfFile> {
         }
         target.next.push(node);
     }
+
+    fn du(&mut self)
+    {
+        let mut total = 0;
+        // if there are kids, recurse and add to running total
+        if self.data.is_dir {
+            for child in &mut self.next {
+                child.du();
+                total += child.data.size;
+            }
+            self.data.size = total;
+        } else {
+            // its a leaf and has a size already
+            // total = self.data.size;
+        }
+        // println!("TOTAL {} {}", total, self.data.name);
+    }
+
+    fn du_stats(& self, path: Option<String>) -> Vec<ElfFile>
+    {
+        let p = path.unwrap_or("".to_string()) + &self.data.name;
+        let mut output = Vec::new();
+        output.push(ElfFile::new(self.data.size, p.clone(), self.data.is_dir));
+        // println!("{} {}", p, self.data.size);
+        for node in &self.next {
+            if node.data.is_dir {
+                let mut leafs = node.du_stats(Some(p.clone()));
+                output.append(& mut leafs);
+            }
+        }
+        output
+    }
 }
 
 
@@ -71,7 +103,7 @@ fn part_one() {
     println!("Part one");
 
     let root_node = ElfFile::new(0, "/".to_string(), true);
-    let mut filesystem = Node::<ElfFile>::new(root_node);
+    let mut filesystem = Node::new(root_node);
     let mut current_dir: Vec<String> = Vec::new();
 
     // construct a tree
@@ -99,20 +131,14 @@ fn part_one() {
                     "dir" => {
                         let dirname = cmd_tokens.next().unwrap();
                         let newelf = ElfFile::new(0, dirname.to_string(), true,);
-                        let filesystem_node: Node<ElfFile> = Node {
-                            next: Vec::new(),
-                            data: Box::new(newelf),
-                        };
+                        let filesystem_node = Node::new(newelf);
                         filesystem.push(filesystem_node, &current_dir);
                     },
                     c @ &_ => {
                         let size = c.parse::<u32>().unwrap();
                         let name = cmd_tokens.next().unwrap();
                         let newelf = ElfFile::new(size, name.to_string(), false,);
-                        let filesystem_node: Node<ElfFile> = Node {
-                            next: Vec::new(),
-                            data: Box::new(newelf),
-                        };
+                        let filesystem_node = Node::new(newelf);
                         filesystem.push(filesystem_node, &current_dir);
                     },
                 }
@@ -120,19 +146,34 @@ fn part_one() {
         }
     }
     // At this point the filesystem in the data file is constructed.
-    println!("filesystem: {:#?}", filesystem);
+    // println!("filesystem: {:#?}", filesystem);
 
     // Need to walk the tree and total up the sizes.
+    filesystem.du();
+
+    // println!("filesystem: {:#?}", filesystem);
+
+    let mut stats = filesystem.du_stats(None);
+    stats.retain(|x| x.size <= 100000);
+    let mut score = 0;
+    for stat in stats {
+        score += stat.size;
+    }
+    println!("score: {}", score);
+
+    println!("Part two");
+
+    let used_space = filesystem.data.size;
+    let free_space = 70000000 - used_space;
+    let space_needed = 30000000 - free_space;
+
+    stats = filesystem.du_stats(None);
+    stats.retain(|x| x.size > space_needed);
+    stats.sort_by(|a, b| a.size.cmp(&b.size));
+    score = stats[0].size;
+    println!("score: {}", score);
 
 }
 
 fn part_two() {
-    println!("Part two");
-
-    if let Ok(lines) = read_lines("./inputs/input-day7.txt") {
-        for line in lines {
-            if let Ok(ip) = line {
-            }
-        }
-    }
 }
